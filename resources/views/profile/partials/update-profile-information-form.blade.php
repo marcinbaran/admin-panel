012c9bfd5d6fc8be848b24901b37cee62cf4a292
Revi <mrevib@gmail.com>
2024-09-26 22:16:34 +0200

    Added avatar & changed name to first_name and added last_name

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,14 +13,22 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
         @method('patch')
 
+        <!-- First Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            <x-input-label for="first_name" :value="__('First Name')" />
+            <x-text-input id="first_name" name="first_name" type="text" class="mt-1 block w-full" :value="old('first_name', $user->first_name)" required autofocus autocomplete="given-name" />
+            <x-input-error class="mt-2" :messages="$errors->get('first_name')" />
+        </div>
+
+        <!-- Last Name -->
+        <div>
+            <x-input-label for="last_name" :value="__('Last Name')" />
+            <x-text-input id="last_name" name="last_name" type="text" class="mt-1 block w-full" :value="old('last_name', $user->last_name)" required autocomplete="family-name" />
+            <x-input-error class="mt-2" :messages="$errors->get('last_name')" />
         </div>
 
         <div>
@@ -45,6 +53,19 @@
                     @endif
                 </div>
             @endif
+        </div>
+
+        @if ($user->avatar)
+            <div>
+                <p>{{ __('Current Avatar:') }}</p>
+                <img src="{{ Storage::url($user->avatar) }}" alt="Avatar" class="mt-2 w-24 h-24 rounded-full">
+            </div>
+        @endif
+
+        <div>
+            <x-input-label for="avatar" :value="__('Avatar')" />
+            <input type="file" id="avatar" name="avatar" accept="image/*" class="mt-1 block w-full">
+            <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
         </div>
 
         <div class="flex items-center gap-4">
