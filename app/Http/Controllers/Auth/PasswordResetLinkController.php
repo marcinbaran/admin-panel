c4e46791291e023805715e8e27f9276f7f9b2631
Revi <mrevib@gmail.com>
2024-09-26 21:11:08 +0200

    Added custom password reset template && added mailtrap for testing

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\PasswordResetMail;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
 
@@ -29,16 +32,16 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+        $user = User::where('email', $request->email)->first();
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                        ->withErrors(['email' => __($status)]);
+        if ($user) {
+            $token = Password::createToken($user);
+            Mail::to($user->email)->send(new PasswordResetMail($token, $user->email));
+
+            return back()->with('status', __('messages.reset_link_send_success'));
+        }
+
+        return back()->withInput($request->only('email'))
+            ->withErrors(['email' => __('messages.reset_link_send_error')]);
     }
 }
---
+++ app/Mail/PasswordResetMail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $token;
+    public $email;
+
+    public function __construct($token, $email)
+    {
+        $this->token = $token;
+        $this->email = $email;
+    }
+
+    public function build()
+    {
+        return $this->subject(__('messages.reset_subject'))
+            ->view('emails.password_reset')
+            ->with([
+                'token' => $this->token,
+                'email' => $this->email,
+            ]);
+    }
+}
